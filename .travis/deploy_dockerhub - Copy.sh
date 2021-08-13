02fddfcb9d7f27b8586b6cf482abe5a5aefe9a97
sifubro <theokasio@gmail.com>
2021-08-13 21:44:11 +0100

    Add AWS Dockerrun for deployment!

--- .travis/deploy_dockerhub - Copy.sh
+++
@@ -1,4 +0,0 @@
-docker login --username $DOCKER_USER --password $DOCKER_PASS
-docker build -f Dockerfile -t flask_docker_aws_app .
-docker tag flask_docker_aws_app $DOCKER_REPO:latest
-docker push $DOCKER_REPO:latest
