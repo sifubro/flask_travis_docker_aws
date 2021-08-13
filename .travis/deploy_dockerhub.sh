cd18ad01b936281a573e00cdde5679cadc7101de
sifubro <theokasio@gmail.com>
2021-08-13 19:32:22 +0100

    TravisCI automatic deploys to DockerHub v7 - changed the deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,4 +1,4 @@
-docker login --username "sifubro" --password "a270188ma$$$"
+docker login --username sifubro --password $DOCKER_PASS
 docker build -f Dockerfile -t flask_docker_aws_app .
 docker tag flask_docker_aws_app sifubro/flask_travis_docker_aws:latest
 docker push sifubro/flask_travis_docker_aws:latest
